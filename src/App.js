e283c515e473824b3f59e291ce7f2e59837fa5b8
minhyungkoo <minhyungkoo@naver.com>
2020-10-01 17:21:09 +0900

    prop-check

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React from 'react';
+import Proptypes from 'prop-types';
 
-function FOOD({ name, picture }){
+function Food({ name, picture, rating }){
     return <div>
       <h2>I like {name} </h2>
-      <img src={picture} />
+      <img src={picture} alt={name} />
+      <h4> {rating}/5 </h4>
     </div>;
 }
 
+
 const foodIlike = [
-  {name: 'Kimchi',
-  image : 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Various_kimchi.jpg/330px-Various_kimchi.jpg'
+  {id: 1,
+  name: 'Kimchi',
+  image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Various_kimchi.jpg/330px-Various_kimchi.jpg',
+  rating: 3.9
   }
 ]
 
+Food.propTypes = {
+  name: Proptypes.string.isRequired,
+  picture: Proptypes.string.isRequired,
+  rating: Proptypes.number
+};
+
+function renderFood(dish){
+  return <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />;
+}
 
   function App() {
     return (
       <div>
         <h1>HELLO</h1>
-        {foodIlike.map(dish => <FOOD name={dish.name} picture={dish.image}/>)}
-        <FOOD name='test food' /> 
+        {console.log(foodIlike.map(renderFood))}
+        {foodIlike.map(renderFood)} 
       </div>
     ); 
   }
