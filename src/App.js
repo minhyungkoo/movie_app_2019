bc4bc0858aa7b9690a89e5e951fe4eff0aa914a7
minhyungkoo <minhyungkoo@naver.com>
2020-10-01 15:23:09 +0900

    auto

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with minhyung
-          test 2 
-        </a>
-      </header>
-    </div>
-  );
+function FOOD({ name, picture }){
+    return <div>
+      <h2>I like {name} </h2>
+      <img src={picture} />
+    </div>;
 }
 
+const foodIlike = [
+  {name: 'Kimchi',
+  image : 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Various_kimchi.jpg/330px-Various_kimchi.jpg'
+  }
+]
+
+
+  function App() {
+    return (
+      <div>
+        <h1>HELLO</h1>
+        {foodIlike.map(dish => <FOOD name={dish.name} picture={dish.image}/>)}
+      </div>
+    ); 
+  }
+
 export default App;
+
