715253fcfdc06f05f37c4c02673282b3c095a28b
minhyungkoo <minhyungkoo@naver.com>
2020-10-04 17:40:50 +0900

    css_styling

--- src/Movie.js
+++ src/Movie.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import "./Movie.css";
+import "./App.css";
 
-
-function Movie({id, year, title, summary, poster,genres}){
+function Movie({id, year, title, summary, poster, genres}){
     return(
-        <div className='moive'>
-            <img src={poster} alt={title} title={title} />    
-            <div className='movie'>
+        <div className='movie'>
+            <img src={poster} alt={title} title={title}></img> 
+            <div className='movie__data'>
                 <h3 className='movie__title'>{title}</h3>
                 <h5 className='movie__year'>{year}</h5>
-                <ul className='genres'>
+                <ul className='movie__genres'>
                     {genres.map((genre,index) => (
-                        <li key={index} className='genres__genre'>{genre}</li>
+                        <li key={index} className='genres__genre'>
+                            {genre}
+                        </li>
                     ))}
                 </ul>
                 <p className='movie__summary'>{summary}</p>
-                
             </div>
         </div>
     );
